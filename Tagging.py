842d1fb7694206a5ddc2d638dacd1f4ca5c58b2f
Felix Patzschke <felix.patzschke@gmail.com>
2021-09-19 17:18:04 +0200

    Update Tagging.py
    Initialize the content of some UI elements.

--- Tagging.py
+++ Tagging.py
@@ -10,6 +10,24 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = mainwindow.Ui_MainWindow()
         self.ui.setupUi(self)
+        
+        self.initialize_io_widgets()
+    
+    def initialize_io_widgets(self):
+        # Import Tab
+        self.ui.treeWidget.clear()
+        for btn in [ self.ui.pushButton_3, self.ui.pushButton_4, self.ui.pushButton_5 ]:
+            btn.setEnabled(False)
+        
+        # Alignment Tab
+        # nothing to do here, example values are already reasonable
+        
+        # Type and Corrections Tab
+        self.ui.listWidget.clear()
+        # example values in the input fields for particle info are already resonable.
+        
+        # Export Tab
+        # nothing to do here, example values are already reasonable
 
 # ENTRY POINT
 if __name__ == '__main__':
