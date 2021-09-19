e63d61c3ab6a2a0c60b47ac6f7a5cc9d9ec0e8e8
Felix Patzschke <felix.patzschke@gmail.com>
2021-09-19 16:59:28 +0200

    Create Tagging.py
    This is the program that I'll ultimately want to use to tag particles for later analysis.
    
    Its only functionality for now is to open up the GUI, though.

---
+++ Tagging.py
@@ -0,0 +1,21 @@
+
+import os
+import sys
+
+from PyQt5 import QtWidgets
+from  ui import mainwindow
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = mainwindow.Ui_MainWindow()
+        self.ui.setupUi(self)
+
+# ENTRY POINT
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
